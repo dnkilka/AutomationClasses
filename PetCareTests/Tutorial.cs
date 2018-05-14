89c5931161afbc6d6d6074f32157d8bb5690427e
Iryna Shcherbyna <dnk.ilka@gmail.com>
2018-05-14 14:57:07 -0500

    Testing committing and pulling changes

---
+++ PetCareTests/Tutorial.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PetCareTests
+{
+    [TestFixture]
+    public class Tutorial
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
