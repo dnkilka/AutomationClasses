c344b06a8e930df801eedae837fe2190429087ec
Iryna Shcherbyna <dnk.ilka@gmail.com>
2018-05-15 13:16:21 -0500

    Added parameters for values we use in the test

--- PetCareTests/Tests/CareRequestTest.cs
+++ PetCareTests/Tests/CareRequestTest.cs
@@ -18,26 +18,34 @@ namespace PetCareTests.Tests
             driver.Navigate().GoToUrl("http://nitro.duckdns.org/Pets/careRequest.html");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
+            var customerFirstName = "Iryna";
+            var customerLastName = "Shch";
+            var customerPhoneNumber = "2244225588";
+            var customerEmail = "someEmail@gmail.com";
+            var catsNumber = "2";
+            var otherNumber = "3+";
+            var comment = "Please be quiet, our spiders are easily scared";
+
             //Fill out inputs
-            driver.FindElement(By.Id("firstName")).SendKeys("Iryna");
-            driver.FindElement(By.Name("lastName")).SendKeys("Xxx");
-            driver.FindElement(By.ClassName("phoneNumber")).SendKeys("2244225588");
-            driver.FindElement(By.XPath("//label[.='Email']/parent::div[1]/following-sibling::div[1]//input")).SendKeys("someEmail.com");
+            driver.FindElement(By.Id("firstName")).SendKeys(customerFirstName);
+            driver.FindElement(By.Name("lastName")).SendKeys(customerLastName);
+            driver.FindElement(By.ClassName("phoneNumber")).SendKeys(customerPhoneNumber);
+            driver.FindElement(By.XPath("//label[.='Email']/parent::div[1]/following-sibling::div[1]//input")).SendKeys(customerEmail);
 
             //Click Animal Type checkboxes
             driver.FindElement(By.Id("cat")).Click();
             var catQuantitySelect = new SelectElement(driver.FindElement(By.Id("catQuantity")));
-            catQuantitySelect.SelectByText("2");
+            catQuantitySelect.SelectByText(catsNumber);
 
             driver.FindElement(By.Id("other")).Click();
             var otherQuantitySelect = new SelectElement(driver.FindElement(By.Id("otherQuantity")));
-            otherQuantitySelect.SelectByText("3+");
+            otherQuantitySelect.SelectByText(otherNumber);
 
             //Visits per day
             driver.FindElement(By.Id("visitSeveralTimesPerDay")).Click();
 
             //Comments
-            driver.FindElement(By.Id("comments")).SendKeys("Please be quiet, our spiders are easily scared");
+            driver.FindElement(By.Id("comments")).SendKeys(comment);
 
             //Click Request button
             driver.FindElement(By.Id("requestButton")).Click();
