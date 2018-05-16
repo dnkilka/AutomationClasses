229b1acc091300c0340a00325db74ff6940691c7
Iryna Shcherbyna <dnk.ilka@gmail.com>
2018-05-16 09:39:15 -0500

    Added page objects

--- PetCareTests/Tests/CareRequestTest.cs
+++ PetCareTests/Tests/CareRequestTest.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium.Chrome;
 using System.Threading;
 using Shouldly;
 using System;
-using OpenQA.Selenium.Support.UI;
+using PetCareTests.Pages;
 
 namespace PetCareTests.Tests
 {
@@ -18,6 +18,8 @@ namespace PetCareTests.Tests
             driver.Navigate().GoToUrl("http://nitro.duckdns.org/Pets/careRequest.html");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
+            var careRequestPage = new CareRequestPage(driver);
+
             var customerFirstName = "Iryna";
             var customerLastName = "Shch";
             var customerPhoneNumber = "2244225588";
@@ -28,61 +30,56 @@ namespace PetCareTests.Tests
             var comment = "Please be quiet, our spiders are easily scared";
 
             //Fill out inputs
-            driver.FindElement(By.Id("firstName")).SendKeys(customerFirstName);
-            driver.FindElement(By.Name("lastName")).SendKeys(customerLastName);
-            driver.FindElement(By.ClassName("phoneNumber")).SendKeys(customerPhoneNumber);
-            driver.FindElement(By.XPath("//label[.='Email']/parent::div[1]/following-sibling::div[1]//input")).SendKeys(customerEmail);
+            careRequestPage.FillOutContactInformation(customerFirstName, customerLastName, customerPhoneNumber, customerEmail);
 
             //Click Animal Type checkboxes
-            driver.FindElement(By.Id("cat")).Click();
-            var catQuantitySelect = new SelectElement(driver.FindElement(By.Id("catQuantity")));
-            catQuantitySelect.SelectByText(catsNumber);
-
-            driver.FindElement(By.Id("other")).Click();
-            var otherQuantitySelect = new SelectElement(driver.FindElement(By.Id("otherQuantity")));
-            otherQuantitySelect.SelectByText(otherNumber);
+            careRequestPage.RequestCatCare(catsNumber);
+            careRequestPage.RequestOtherCare(otherNumber);
 
             //Visits per day
-            driver.FindElement(By.Id("visitSeveralTimesPerDay")).Click();
+            careRequestPage.SetVisitsPerDay(visitsPerDay);
 
             //Comments
-            driver.FindElement(By.Id("comments")).SendKeys(comment);
+            careRequestPage.FillOutComments(comment);
 
             //Click Request button
-            driver.FindElement(By.Id("requestButton")).Click();
+            careRequestPage.ClickRequestButton();
 
             //Verify data on the Request Summary pop-up
-            Assert.IsTrue(driver.FindElement(By.ClassName("summaryBlock")).Displayed);
-            driver.FindElement(By.ClassName("summaryBlock")).Displayed.ShouldBeTrue();
+            var requestSummaryPage = new RequestSummaryPage(driver);
 
-            var header = driver.FindElement(By.Id("myModalLabel")).Text;
+            //Verify Page opened by checking page element is visible
+            Assert.IsTrue(requestSummaryPage.SummaryBlock.Displayed);
+            requestSummaryPage.SummaryBlock.Displayed.ShouldBeTrue();
+
+            //Verify Header text
+            var header = requestSummaryPage.PageHeader.Text;
             Assert.AreEqual("Request Summary", header);
             header.ShouldBe("Request Summary");
 
-            var firstName = driver.FindElement(By.XPath("//div[contains(.,'First Name:') and (contains(@class, 'summaryBlock'))]")).Text.Replace("First Name: ", "");
+            //Verify contact info
+            var firstName = requestSummaryPage.FirstNameRecord.Text.Replace("First Name: ", "");
             Assert.AreEqual(customerFirstName, firstName);
-
-            var lastName = driver.FindElement(By.XPath("//div[contains(.,'Last Name:') and (contains(@class, 'summaryBlock'))]")).Text.Replace("Last Name: ", "");
+            var lastName = requestSummaryPage.LastNameRecord.Text.Replace("Last Name: ", "");
             Assert.AreEqual(customerLastName, lastName);
-
-            var phoneNumber = driver.FindElement(By.XPath("//div[contains(.,'Phone') and (contains(@class, 'summaryBlock'))]")).Text.Replace("Phone #: ", "");
+            var phoneNumber = requestSummaryPage.PhoneNumberRecord.Text.Replace("Phone #: ", "");
             Assert.AreEqual(customerPhoneNumber, phoneNumber);
-
-            var email = driver.FindElement(By.XPath("//div[contains(.,'Email') and (contains(@class, 'summaryBlock'))]")).Text.Replace("Email: ", "");
+             var email = requestSummaryPage.EmailRecord.Text.Replace("Email: ", "");
             Assert.AreEqual(customerEmail, email);
 
-            var allText = driver.FindElement(By.ClassName("modal-content")).Text;
+            //Verify all other information is populated correctly
+            var allText = requestSummaryPage.ModalContent.Text;
             Assert.IsTrue(allText.Contains($"{catsNumber} cat(s)"));
             allText.Contains($"{catsNumber} cat(s)").ShouldBeTrue();
             Assert.IsTrue(allText.Contains($"{otherNumber} other animal(s)"));
             Assert.IsTrue(allText.Contains($"{visitsPerDay} visits per day are required."));
             Assert.IsTrue(allText.Contains(comment));
 
-            //Click Close button
-            driver.FindElement(By.XPath("//button[.='Close']")).Click();
+            //Click Close button and verify the page was closed
+            requestSummaryPage.CloseButton.Click();
             Thread.Sleep(1000);
-            Assert.IsFalse(driver.FindElement(By.ClassName("summaryBlock")).Displayed);
-            driver.FindElement(By.ClassName("summaryBlock")).Displayed.ShouldBeFalse();
+            Assert.IsFalse(requestSummaryPage.SummaryBlock.Displayed);
+            requestSummaryPage.SummaryBlock.Displayed.ShouldBeFalse();
             
             driver.Quit();
         }
