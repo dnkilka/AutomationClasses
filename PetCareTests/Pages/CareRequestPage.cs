02854d0edf4832f21c753584a61634fc4fadddc1
Iryna Shcherbyna <dnk.ilka@gmail.com>
2018-05-16 09:39:15 -0500

    Added page objects

---
+++ PetCareTests/Pages/CareRequestPage.cs
@@ -0,0 +1,95 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareTests.Pages
+{
+    public class CareRequestPage
+    {
+        private readonly IWebDriver _driver;
+
+        // Bad approach (a), because the code will try to find the elements every time when the page is created.
+        // This is bad, because 
+        // - sometimes elements appear when you perform some actions on the page
+        // - you might not need all page elements
+        // private IWebElement FirstName_Input;
+
+        public CareRequestPage(IWebDriver driver)
+        {
+            _driver = driver;
+
+            // (a) continue:
+            // FirstName_Input = _driver.FindElement(By.Id("firstName"));
+        }
+
+        // Good approach, but difficult syntax
+        /*private IWebElement FirstNameInput { get
+            {
+                return _driver.FindElement(By.Id("firstName"));
+            }
+        }*/
+
+        // Good approach with good syntax
+        private IWebElement FirstNameInput => _driver.FindElement(By.Id("firstName"));
+        private IWebElement LastNameInput => _driver.FindElement(By.Name("lastName"));
+        private IWebElement PhoneNumberInput => _driver.FindElement(By.ClassName("phoneNumber"));
+        private IWebElement EmailInput => _driver.FindElement(By.XPath("//label[.='Email']/parent::div[1]/following-sibling::div[1]//input"));
+        private IWebElement CatCheckbox => _driver.FindElement(By.Id("cat"));
+        private SelectElement CatQuantityDropdown => new SelectElement(_driver.FindElement(By.Id("catQuantity"));
+        private IWebElement OtherCheckbox => _driver.FindElement(By.Id("other"));
+        private SelectElement OtherQuantityDropdown => new SelectElement(_driver.FindElement(By.Id("otherQuantity")));
+        private IWebElement OneVisitPerDayRadio => _driver.FindElement(By.Id("visitOneTimePerDay"));
+        private IWebElement SeveralVisitsPerDayRadio => _driver.FindElement(By.Id("visitSeveralTimesPerDay"));
+        private SelectElement VisitsPerDayDropdown => new SelectElement(_driver.FindElement(By.Id("visitsPerDay")));
+        private IWebElement CommentsTextArea => _driver.FindElement(By.Id("comments"));
+        private IWebElement SendRequestButton => _driver.FindElement(By.Id("requestButton"));
+
+        public void FillOutContactInformation(string firstName, string lastName, string phone, string email)
+        {
+            FirstNameInput.SendKeys(firstName);
+            LastNameInput.SendKeys(lastName);
+            PhoneNumberInput.SendKeys(phone);
+            EmailInput.SendKeys(email);
+        }
+
+        public void RequestCatCare(string numberOfCats)
+        {
+            CatCheckbox.Click();
+            CatQuantityDropdown.SelectByText(numberOfCats);
+        }
+
+        public void RequestOtherCare(string numberOfOthers)
+        {
+            OtherCheckbox.Click();
+            CatQuantityDropdown.SelectByText(numberOfOthers);
+        }
+
+        public void SetVisitsPerDay(string numberOfVisits)
+        {
+            if(numberOfVisits == "1")
+            {
+                OneVisitPerDayRadio.Click();
+            }
+            else
+            {
+                SeveralVisitsPerDayRadio.Click();
+                VisitsPerDayDropdown.SelectByText(numberOfVisits);
+            }
+        }
+
+        public void FillOutComments(string comment)
+        {
+            CommentsTextArea.SendKeys(comment);
+        }
+
+        public void ClickRequestButton()
+        {
+            SendRequestButton.Click();
+        }
+
+    }
+}
---
+++ PetCareTests/Pages/RequestSummaryPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareTests.Pages
+{
+    public class RequestSummaryPage
+    {
+        private IWebDriver _driver;
+
+        public RequestSummaryPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebElement ModalContent => _driver.FindElement(By.ClassName("modal-content"));
+        public IWebElement PageHeader => _driver.FindElement(By.Id("myModalLabel"));
+        public IWebElement SummaryBlock => _driver.FindElement(By.ClassName("summaryBlock"));
+        public IWebElement FirstNameRecord => _driver.FindElement(By.XPath("//div[contains(.,'First Name:') and (contains(@class, 'summaryBlock'))]"));
+        public IWebElement LastNameRecord => _driver.FindElement(By.XPath("//div[contains(.,'Last Name:') and (contains(@class, 'summaryBlock'))]"));
+        public IWebElement PhoneNumberRecord => _driver.FindElement(By.XPath("//div[contains(.,'Phone') and (contains(@class, 'summaryBlock'))]"));
+        public IWebElement EmailRecord => _driver.FindElement(By.XPath("//div[contains(.,'Email') and (contains(@class, 'summaryBlock'))]"));
+        public IWebElement CloseButton => _driver.FindElement(By.XPath("//button[.='Close']"));
+    }
+}
