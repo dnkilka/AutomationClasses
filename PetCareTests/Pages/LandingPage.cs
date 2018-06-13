81ae03eb2a4b4d59c01bb07999cc59ef58b83d9b
Tetyana Viytyk <testingtanya@yahoo.com>
2018-06-13 09:07:16 -0500

    Create Page Objects for About me and Landing Pages.

---
+++ PetCareTests/Pages/LandingPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PetCareTests.Pages
+{
+    public class LandingPage
+    {
+        private IWebDriver _driver;
+
+        public LandingPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public IWebElement LandingPageHeader => _driver.FindElement(By.XPath("//h1[contains(., 'Alex's Pet Business')]"));
+        public IWebElement KittenHeader => _driver.FindElement(By.ClassName(“logo”));
+        public IWebElement Paragraph1 => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p"));
+        public IWebElement Paragraph2 => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[2]"));
+        public IWebElement Paragraph3 => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[3]"));
+        public IWebElement ContactLink => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[3]/a"));
+        public IWEbElement CatImage => _driver.FindElement(By.XPath("//div[@src = 'images/hcat.png']"));
+        public IWebElement DogImage => _driver.FindElement(By.XPath("//div[@src = 'images/dog.png']"));
+    }
+}
