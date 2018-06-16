470766b517aa81c08d6e35bbb23d2b7abf53e41c
Tetyana Viytyk <testingtanya@yahoo.com>
2018-06-15 19:14:10 -0500

    Corrected locators in the LandingPage

--- PetCareTests/Pages/LandingPage.cs
+++ PetCareTests/Pages/LandingPage.cs
@@ -16,7 +16,7 @@ namespace PetCareTests.Pages
         public IWebElement Paragraph2 => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[2]"));
         public IWebElement Paragraph3 => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[3]"));
         public IWebElement ContactLink => _driver.FindElement(By.XPath("//div[@class = 'par-size']/p[3]/a"));
-        public IWebElement CatImage => _driver.FindElement(By.XPath("//div[@src = 'images/hcat.png']"));
-        public IWebElement DogImage => _driver.FindElement(By.XPath("//div[@src = 'images/dog.png']"));
+        public IWebElement CatImage => _driver.FindElement(By.XPath("//p/img[@src = 'images/hcat.png']"));
+        public IWebElement DogImage => _driver.FindElement(By.XPath("//p/img[@src = 'images/dog.png']"));
     }
 }
