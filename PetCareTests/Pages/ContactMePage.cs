df2220664600a8d3c8afb91691331e4dd0ae78fa
Inna M <iutest180@gmail.com>
2018-06-21 22:34:14 -0500

    Added Contact Me page smoke test

--- PetCareTests/Pages/ContactMePage.cs
+++ PetCareTests/Pages/ContactMePage.cs
@@ -3,20 +3,46 @@ using OpenQA.Selenium.Support.UI;
 
 namespace PetCareTests.Pages
 {
-    public class ContactMePage
+    public class ContactPage
     {
         private IWebDriver _driver;
         
-        public ContactMePage(IWebDriver driver)
+        public ContactPage(IWebDriver driver)
         {
             _driver = driver;
         }
 
-        private IWebElement ContactHeader => _driver.FindElement(By.TagName("//h1"));
-        private IWebElement ContactEntireContent => _driver.FindElement(By.XPath("//div[@class='contact']"));
-        private IWebElement CareRequestLink => _driver.FindElement(By.XPath("//div[@class='contact']//a[@href='careRequest.html']"));
-        private IWebElement EmailLink => _driver.FindElement(By.XPath("//div[@class='contact']//a[@href='mailto:vova64@gmail.com']"));
-        private IWebElement EnvelopeImage => _driver.FindElement(By.XPath("//img[@src='images/mail.png']")); 
-          
+        public IWebElement ContactHeader => _driver.FindElement(By.TagName("h1"));
+        public IWebElement ContactEntireContent => _driver.FindElement(By.XPath("//div[@class='contact']"));
+        public IWebElement CareRequestLink => _driver.FindElement(By.XPath("//div[@class='contact']//a[@href='careRequest.html']"));
+        public IWebElement EmailLink => _driver.FindElement(By.XPath("//div[@class='contact']//a[@href='mailto:vova64@gmail.com']"));
+        public IWebElement EnvelopeImage => _driver.FindElement(By.XPath("//img[@src='images/mail.png']"));
+
+
+        public string GetHeaderTitle()
+        {
+            return ContactHeader.Text;
+        }
+
+        public string GetEntireContent()
+        {
+            return ContactEntireContent.Text;
+        }
+
+        public string GetCareRequestLink()
+        {
+            return CareRequestLink.GetAttribute("href");
+        }
+
+        public string GetEmailLink()
+        {
+            return EmailLink.GetAttribute("href");
+        }
+
+        public string GetEnvelopeImage()
+        {
+            return EnvelopeImage.GetAttribute("src");
+        }
+
     }
 }
