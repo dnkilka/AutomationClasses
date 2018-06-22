428dfcbd95e6cfd7e24be3d978b2821b6abcc1ff
US\Iryna.Shcherbyna <Iryna.Shcherbyna@safelite.com>
2018-06-22 16:51:28 -0500

    Added Try/catch block

--- PetCareTests/Tests/PricesTest.cs
+++ PetCareTests/Tests/PricesTest.cs
@@ -19,26 +19,36 @@ namespace PetCareTests.Tests
         {
             IWebDriver driver = DriverUtils.CreateDriver();
 	        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-			// Open Landing Page
-			var landingPage = URLs.OpenUrl(driver);
+	        try
+	        {
+				// Open Landing Page
+				var landingPage = URLs.OpenUrl(driver);
            
-			//Open Prices page
-            var pricesPage = landingPage.ClickPricesLink();
+				//Open Prices page
+				var pricesPage = landingPage.ClickPricesLink();
                        
-            //Verify Header text
-            var header = pricesPage.PricesHeader.Text;
-            header.ShouldBe("Prices");
+				//Verify Header text
+				var header = pricesPage.PricesHeader.Text;
+				header.ShouldBe("Prices");
 
-            //Verify paragraph text
-            var paragraph = pricesPage.PricesParagraph.Text;
-            paragraph.ShouldBe("These are my prices for different pets.");
+				//Verify paragraph text
+				var paragraph = pricesPage.PricesParagraph.Text;
+				paragraph.ShouldBe("These are my prices for different pets.");
 
-            //Verify prices list text
-            var list = pricesPage.PricesList.Text;
-            list.ShouldBe("Dogs $10 per day");
-            
-            driver.Quit();
-        }
+				//Verify prices list text
+				var list = pricesPage.PricesList.Text;
+				list.ShouldBe("Dogs $10 per day");
+
+			}
+			catch (Exception e)
+	        {
+		        Console.WriteLine(e);
+		        throw;
+	        }
+	        finally
+	        {
+		        driver.Quit();
+	        }
+		}
     }
 }
