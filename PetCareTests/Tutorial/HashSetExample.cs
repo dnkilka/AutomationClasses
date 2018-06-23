6705f0c1736c484dac94c4497613a97dbaac47e5
dnknitro <vova64@gmail.com>
2018-06-22 20:10:05 -0500

    Added HashSet example

---
+++ PetCareTests/Tutorial/HashSetExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Shouldly;
+
+namespace PetCareTests.Tutorial
+{
+    [TestFixture]
+    internal class HashSetExample
+    {
+        [Test]
+        public void Sets()
+        {
+            var myFirstSet = new HashSet<int>();
+            myFirstSet.Add(1);
+            myFirstSet.Add(3);
+            myFirstSet.Add(1);
+            myFirstSet.Add(int.MaxValue);
+            myFirstSet.Add(int.MinValue);
+            Console.WriteLine(myFirstSet.Count);
+            foreach (var setItem in myFirstSet)
+            {
+                Console.WriteLine(setItem);
+            }
+            myFirstSet.Count.ShouldBe(4);
+        }
+    }
+}
